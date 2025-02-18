e8a7476434032d03cdf044901763b0afcc9614c1
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-02-18 22:18:06 +0000

    new and create in tacogram

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,18 @@ class PostsController < ApplicationController
     # render posts/new view with new Post form
   end
 
-  # def create
-  #   # start with a new Post
-  #   # assign user-entered form data to Post's columns
-  #   # save Post row
-  #   # redirect user
-  # end
+  def create
+    # start with a new Post
+    @post = Post.new
+
+    # assign user-entered form data to Post's columns
+    @post["author"] = params["author"]
+    @post["image"] = params["image"]
+    @post["body"] = params["body"]
+    # save Post row
+    @post.save
+    # redirect user
+    redirect_to "/posts"
+  end
 
 end
