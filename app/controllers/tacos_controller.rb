60fb6ffebfe77bc28fd824fb04701a8bf61809fe
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-02-18 20:07:29 +0000

    refactoring practice

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,6 +1,8 @@
 class TacosController < ApplicationController
 
   def index
+    @fillings = ["Carnitas", "Al Pastor", "Steak", "Fish", "Veggie","Choco"]
+
     # render :template => "tacos/index"
   end
 
